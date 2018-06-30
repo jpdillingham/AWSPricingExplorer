3e9f76c7c36d38b0a017aa307f753f80c1ed7c88
JP Dillingham <jpdillingham@gmail.com>
2018-06-30 15:50:15 -0500

    add blueprint basics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/normalize.css/normalize.css'
+import '../node_modules/@blueprintjs/core/lib/css/blueprint.css'
+import '../node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css'
+
+import { Button, Intent, Spinner } from "@blueprintjs/core";
 
 class App extends Component {
   render() {
@@ -13,6 +19,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Spinner intent={Intent.PRIMARY} />
+        <Button intent={Intent.SUCCESS}>button content</Button>
       </div>
     );
   }
