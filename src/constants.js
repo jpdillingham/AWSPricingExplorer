f3eb7755ad87149ba59fcf6b6a3869c38b1f0736
JP Dillingham <jpdillingham@gmail.com>
2018-07-01 17:28:43 -0500

    update location of dotenv

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('./dotenv').config();
 
 export const BACKEND_PORT = process.env.REACT_APP_BACKEND_PORT || 3001;
 export const BACKEND_URL = 'http://localhost:' + BACKEND_PORT;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('./dotenv').config();
 const AWS = require('aws-sdk');
 const express = require('express');  
 const bodyParser = require('body-parser');  
