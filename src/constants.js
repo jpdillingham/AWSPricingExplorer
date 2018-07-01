acc8b798f051ecb24942df652304fd094ccdc839
JP Dillingham <jpdillingham@gmail.com>
2018-07-01 17:29:56 -0500

    add environment setting for root url

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 require('./dotenv').config();
 
+export const ROOT_URL = process.env.REACT_APP_ROOT_URL || 'http://localhost';
 export const BACKEND_PORT = process.env.REACT_APP_BACKEND_PORT || 3001;
-export const BACKEND_URL = 'http://localhost:' + BACKEND_PORT;
+export const BACKEND_URL = ROOT_URL + ':' + BACKEND_PORT;
