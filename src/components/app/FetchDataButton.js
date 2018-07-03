ed35fa693bc9ab4e5ed0745a62a4f66c06d091c1
JP Dillingham <jpdillingham@gmail.com>
2018-07-02 22:13:34 -0500

    move FetchDataButton css to index

--- src/components/app/FetchDataButton.js
+++ src/components/app/FetchDataButton.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react';
 import { Button } from "@blueprintjs/core";
 
-const styles = {
-	button: {
-		marginLeft: 5,
-		width: 620,
-	},
-}
-
 class FetchDataButton extends Component {
 	render() {
 		return (
 			<label className="pt-label">
 				<span className={'form-label'}></span>
 				<Button 
-					style={styles.button}
 					onClick={this.props.onClick} 
-					className={'pt-intent-success'} 
+					className={'pt-intent-success fetch-data-button'} 
 					icon={'circle-arrow-down'}
 				>
 					Fetch Data
